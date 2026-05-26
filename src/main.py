e8560c5903073f9a59030d2b91191974508059e0
Joao <jomasii2@gmail.com>
2026-05-26 08:49:42 -0300

    feat: layers model implementation #2 #3

--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
 from persistence.account_repository import AccountRepository
 from domain.bank_service import BankService
-from presentation.bank_cli import (
-    BankCLI,
-    run_registration,
-    run_render_interest,
-    run_deposit,
-    run_transfer,
-    run_withdrawal,
-    show_balance,
-)
+from presentation.bank_cli import BankCLI, run_registration, show_balance
 
 if __name__ == "__main__":
     storage = AccountRepository()
@@ -17,11 +9,7 @@ if __name__ == "__main__":
     
     cli = BankCLI("--- Top-Bank CLI ---")
     cli.add_option("1", "Criar Conta", run_registration)
-    cli.add_option("2", "Ver Saldo", show_balance)
-    cli.add_option("3", "Realizar Deposito", run_deposit)
-    cli.add_option("4", "Realizar Saque", run_withdrawal)
-    cli.add_option("5", "Realizar Transferência", run_transfer)
-    cli.add_option("6", "Render Juros", run_render_interest)
+    cli.add_option("2", "Consultar Saldo", show_balance)
     cli.add_option("0", "Sair", lambda s: False)
     
     cli.run(service)
